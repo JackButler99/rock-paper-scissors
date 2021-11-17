d13e649a4575e5679ab169dc3d4530a84f179187
JackButler99 <ridhokurniawan32@gmail.com>
2021-11-17 16:15:48 +0700

    code for basic javascript rock-paper-scissors game

---
+++ index.js
@@ -0,0 +1,96 @@
+const options = ["rock", "paper", "scissor"]
+let player 
+let cpu 
+let score={}
+
+const computerPlay=()=>{
+  let index = Math.floor(Math.random()*options.length) 
+  let computerSelection = {option: options[index], number: index}
+  console.log(computerSelection)
+  return(computerSelection)
+}
+
+const playerPlay=()=>{
+  let index = Number(window.prompt("Select: 1. rock, 2.paper, 3.scissor", ""))
+  if (index != 1 && index != 2 && index != 3){
+      window.alert('Please only input number 1-3')
+      let playerSelection = 'undefined'
+      console.log(playerSelection)
+      return(playerSelection)
+    }else{
+      let playerSelection = {option: options[index-1], number: index-1}    
+      console.log(playerSelection)
+      return(playerSelection)
+    }
+   
+}
+
+const winner = (p1, p2) =>{
+  if (player=='undefined' || p1 == -1){
+    console.log("Error in user input")
+    return("Error")
+  }
+  if ((p1 + 1) % 3 == p2){
+    score.cpu += 1
+    console.log(`${cpu.option} beat ${player.option}, Computer Won`)
+    return score
+  }else if(p1 == p2){
+    console.log(`${player.option} vs ${cpu.option}, it is a draw`)
+    return score 
+  }else{
+    score.player += 1
+    console.log(`${player.option} beat ${cpu.option}, You Won`)
+    return score
+  } 
+  
+}
+
+  
+
+//Main Loop
+const game =()=>{
+  const totalRound = Number(window.prompt("How many round will we play? (Max.10  round)", 5))
+  console.log("Total Round: ", totalRound)
+  if (totalRound>10 ||isNaN(totalRound)){
+    console.log("error on total Round Input")  
+  }
+  else{  
+    score={player: 0, cpu: 0}
+    let round = 1
+    while (round<=totalRound){
+      console.log(`round ${round}`)
+      player = playerPlay()
+      cpu = computerPlay()
+      let roundWinner = winner (player.number, cpu.number)
+      console.log(roundWinner)
+      if (round == totalRound){
+        if (roundWinner.player > roundWinner.cpu) {
+          console.log("Congratulations, You Won !!")
+          }
+        else if (roundWinner.player < roundWinner.cpu){
+          console.log("You Lose !!!")
+        }
+        else if (roundWinner.player == roundWinner.cpu){
+          console.low("It's a Tie !!, Refresh page to Play Again")
+        }
+      }
+      if (roundWinner != "Error"){
+        round +=1 
+      }    
+    }
+  }
+}
+
+
+game()
+
+
+
+
+    
+
+
+
+       
+
+        
